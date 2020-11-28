1d25bde209ff431132847c50b952eab15b1a2e40
Bostoen <32932558+Bostoen@users.noreply.github.com>
2020-11-28 17:20:38 +0100

    Init commits

--- code/data_structures.py
+++ code/data_structures.py
@@ -1,27 +1,81 @@
+from abc import abstractmethod
 """
 This file contains the data structures used in the program.
 """
 
 
-class Derivative:
-    """
-    Base class for all derivatives
-    """
-    def __init__(self, type):
+class Instrument:
+    @abstractmethod
+    def payoff(self, price: float) -> float:
+        """
+        Function that gives the payout of the instrument
+        """
         pass
 
-    def payoff(self):
-        pass
 
-    pass
+class Stock(Instrument):
+    """
+    ticker: Stock ticker
+    price: Last price of the stock
+    vol: last volatility of the stock
+    """
+    def __init__(self, ticker: str, price: float, vol: float):
+        self.ticker = ticker
+        self.price = price
+        self.vol = vol
+
+    def payoff(self, closing_price: float) -> float:
+        return closing_price - self.price
 
 
-class Option(Derivative):
-    pass
+class Call(Instrument):
+    """
+    stock: The underlying
+    strike: Strike of the option
+    dte: Days until expiry
+    """
+    def __init__(self, stock: Stock, strike: float, dte: int):
+        self.strike = strike
+        self.dte = dte
+        super().__init__(stock)
+
+    def payoff(self, closing_price):
+        return max(.0, (closing_price-self.strike)*100)
+
+
+class Put(Instrument):
+    """
+    stock: The underlying
+    strike: Strike of the option
+    dte: Days until expiry
+    """
+    def __init__(self, stock: Stock, strike: float, dte: int):
+        self.strike = strike
+        self.dte = dte
+        super().__init__(stock)
+
+    def payoff(self, closing_price):
+        return max(.0, (self.strike-closing_price)*100)
+
+
+####################
+#    STRATEGIES    #
+####################
 
 
 class Strategy:
     """
-    A strategy consists of any number of derivatives traded as a single block.
+    A strategy consists of any number of instruments traded as a single block.
     """
-    pass
+    def __init__(self, *instruments: tuple):
+        self.legs = set()
+        for instrument in instruments:
+            self.add_leg(instrument[0], instrument[1])
+
+    def add_leg(self, instrument, amount):
+        self.legs.add(self.Leg(instrument, amount))
+
+    class Leg:
+        def __init__(self, pos: Instrument, amount: int):
+            self.position = pos
+            self.amount = amount
