bbf4098e6babb1cd296abbf8b7c3d38af40ccc7b
Worst_Programmer_Ever <57257715+worst-programmer-ever@users.noreply.github.com>
2020-11-29 18:03:06 +0100

    Update functions.py

--- code/functions.py
+++ code/functions.py
@@ -1,21 +1,44 @@
 from code.data_structures import *
+import numpy as np
+
 """
 This file contains the functions that do the necessary work.
 """
-BASE_MONTE_CARLO_SIZE = 1000
+variables = {"r": 0.05, "n": 50000}
+np.random.seed(1)
 
 
-def monte_carlo_instrument(instrument: Instrument, n=BASE_MONTE_CARLO_SIZE) -> float:
+# instrument: Instrument
+def monte_carlo_instrument(instrument_vol, S0: float, month_to_maturity) -> float:
     """
     Approximate the price of an instrument via Monte Carlo
     """
-    pass
+    M = 12 #interval for discretization
+    T = month_to_maturity
+    
+    def mcs(M,n):   
+        dt = T / M
+        S = np.zeros((M + 1, n))
+        S[0] = S0
+        rn = np.random.standard_normal(S.shape)
+        for t in range(1, M+1):
+            S[t]=S[t-1]*np.exp((variables['r']-instrument_vol**2/2)*dt+instrument_vol*np.sqrt(dt)*rn[t])
+        return S
+    
+    S = mcs(M,variables['n'])
+    print(S[-1].mean())
+    print(S0*np.exp(variables['r']*T))
+    C0 = np.exp(-variables['r'] * T)*np.maximum(S0 - S[-1],0).mean()
+            
+    return C0
 
 
-def monte_carlo_strategy(strategy: Strategy, n=BASE_MONTE_CARLO_SIZE) -> float:
+def monte_carlo_strategy(strategy: Strategy) -> float:
     """
     Approximate the price of a strategy via Monte Carlo
     """
+    
+    
 
     # idée en pseudocode:
     #
@@ -28,3 +51,8 @@ def monte_carlo_strategy(strategy: Strategy, n=BASE_MONTE_CARLO_SIZE) -> float:
     #    + put_1.payoff() * 2
     #    + call_2.payoff() * -1
     pass
+
+
+
+test = monte_carlo_instrument(0.2, 100, 12)
+print(test)
